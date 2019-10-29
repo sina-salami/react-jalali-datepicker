53106a3b17d6ad15377d7eea523173d0056866d8
sina-salami <41231264+sina-salami@users.noreply.github.com>
2019-10-29 15:48:35 +0330

    Update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -41,49 +41,49 @@ export const defaultRangeTheme: IRangeDatePickerTheme = {
   cancelHoverColor: "#000",
 };
 
-export const defaultDatePickerTheme: IDatePickerTheme = {
-  backColor: "#FFFFFF",
-  // head
-  headBackColor: "#FFFFFF",
-  headTitleColor: "#aeaeae",
-  headTimeTitleColor: "#617fdf",
-  headArrowColor: "#000",
-  headRangeBackColor: "#D6D6D6",
-  headRangeColor: "#000",
-
-  // weekdays color
-  weekDaysColor: "#3F3F3F",
-
-  // days
-  daysColor: "#000",
-  daysBackColor: "#FFFFFF",
-  holidaysColor: "#edb53b",
-  holidaysBackColor: "#FFFFFF",
-  daysRound: "50%",
-
-  selectDayColor: "#fff",
-  selectDayBackColor: "#617fdf",
-
-  // buttons
-  submitBackColor: "#D6D6D6",
-  submitHoverBackColor: "#f3f3f3",
-  submitColor: "#000",
-  submitHoverColor: "#000",
-  cancelBackColor: "#fff",
-  cancelHoverBackColor: "#D6D6D6",
-  cancelColor: "#000",
-  cancelHoverColor: "#000",
-  changeViewButtonBackColor: "#D6D6D6",
-  changeViewButtonHoverBackColor: "#fff",
-  changeViewButtonColor: "#000",
-  changeViewButtonHoverColor: "#617fdf",
-  // time
-  timeBackColor: "#f0f0f0",
-  timeNumberColor: "#000",
-  handBackColor: "#617fdf",
-  handCircleColor: "#617fdf",
-  selectedNumberColor: "#fff",
-};
+export const IDatePickerTheme = {
+    backColor: "#624E9C",
+    // head
+    headBackColor: "#624E9C",
+    headTitleColor: "#ffff",
+    headArrowColor: "#fff",
+    headTimeTitleColor: "#fff",
+    headRangeBackColor: "#fff",
+    headRangeColor: "#000",
+  
+    // weekdays color
+    weekDaysColor: "#fff",
+  
+    // days
+    daysColor: "#000",
+    daysBackColor: "#FFFFFF",
+    holidaysColor: "#FF4949",
+    holidaysBackColor: "#FFFFFF",
+    daysRound: "50%",
+  
+    selectDayColor: "#fff",
+    selectDayBackColor: "#617fdf",
+  
+    // buttons
+    submitBackColor: "#fff",
+    submitHoverBackColor: "#D6D6D6",
+    submitColor: "#000",
+    submitHoverColor: "#000",
+    cancelBackColor: "#fff",
+    cancelHoverBackColor: "#D6D6D6",
+    cancelColor: "#000",
+    cancelHoverColor: "#000",
+    changeViewButtonBackColor: "#fff",
+    changeViewButtonHoverBackColor: "#D6D6D6",
+    changeViewButtonColor: "#000",
+    changeViewButtonHoverColor: "#000",
+    // time
+    timeBackColor: "#f0f0f0",
+    timeNumberColor: "#000",
+    handBackColor: "#617fdf",
+    handCircleColor: "#617fdf",
+    selectedNumberColor: "#fff",
+  };
 
 const {
   default: styled,
